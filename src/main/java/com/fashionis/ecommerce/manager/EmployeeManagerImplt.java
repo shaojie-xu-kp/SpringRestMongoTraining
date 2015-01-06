9799865a1bc33f6f42c9a5883a658f210762c254
XuShaojie <xsj8585@hotmail.com>
2015-01-06 19:16:03 +0100

    remove the override annotation

--- src/main/java/com/fashionis/ecommerce/manager/EmployeeManagerImplt.java
+++ src/main/java/com/fashionis/ecommerce/manager/EmployeeManagerImplt.java
@@ -12,38 +12,34 @@ public class EmployeeManagerImplt implements EmployeeManager {
 	@Autowired
 	private EmployeeRepo employeeRepo;
 
-	
-	@Override
 	public Employee findEmployeeById(Integer id) {
 		Employee emFound = this.employeeRepo.findById(id);
 		return emFound;
 	}
 
-	@Override
 	public Employee Create(Employee employee) {
 		Employee emSaved = this.employeeRepo.save(employee);
 		return emSaved;
 	}
 
-	@Override
 	public List<Employee> getAllEmployees() {
 		
 		return this.employeeRepo.findAll();
 	}
 
-	@Override
 	public Employee deleteById(Integer id) {
 		Employee empToDelete = this.employeeRepo.findById(id);
 		this.employeeRepo.delete(id);
 		return empToDelete;
 	}
 
-	@Override
 	public Employee updateEmploye(Integer id, Employee employee) {
 		Employee emp = this.employeeRepo.findById(id);
 		emp.setName(employee.getName());
 		emp.setCreatedDate(employee.getCreatedDate());
 		return this.employeeRepo.save(emp);
 	}
+
+	
 
 }
